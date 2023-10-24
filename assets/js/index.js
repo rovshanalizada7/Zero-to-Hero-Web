ddc5d11a6e03efbfb4eedd929725d6aa95bff7be
Rovshan Alizada <rovshanalizada@gamil.com>
2023-10-24 10:42:37 +0400

    JS file pulled out

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+const name = "Ahmet";
+        var surname = "Yilmaz";
+        var fullname = name + " " + surname ;
+        var age = 18 ;
+        var teenager = age !== "24" ;
+        var thisYear = 2023 ;
+        //  name = "Mehmet";
+        console.log(age>17 && age < 20)
+        console.log(teenager);
+        console.log("HELLO WORLD!");
+        console.log(++ age  );
+        console.log (name,surname);
+        console.log (fullname,age);
+        console.log ("Birth date:",thisYear - age);
